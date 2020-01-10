c4de4b227629705e357814932e5624ac5bcfb948
The_Tenth <joseph.justin.diaz@gmail.com>
2020-01-10 10:35:02 -0500

    Initial commit

---
+++ src/Convert.java
@@ -0,0 +1,10 @@
+public class Convert {
+    public static void main(String[] args)
+    {
+        String trat = "Racecar";
+
+        StringBuffer trt = new StringBuffer(trat);
+        trt.reverse();
+        System.out.println(trt);
+    }
+}
---
+++ src/reverseCurse.java
@@ -0,0 +1,13 @@
+//https://www.geeksforgeeks.org/reverse-a-string-in-java/
+
+public class reverseCurse {
+    public static void main (String[] args)
+    {
+        String gd = "god";
+
+        StringBuffer str1 = new StringBuffer(gd);
+        str1.reverse();
+        System.out.println(str1);
+    }
+
+}
